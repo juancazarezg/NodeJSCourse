65c4dab01a51641e54cf07fc40ac92d245cac465
Juan Cazarez <juanjose.cazarezg@gmail.com>
2021-02-15 16:25:10 -0600

    Mongo node example 2

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operation')
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
@@ -8,26 +9,24 @@ MongoClient.connect(url, (err, client) => {
     assert.strictEqual(err, null);
     console.log('Connected correctly to server');
     const db = client.db(dbname);
-    const collection = db.collection('dishes');
+    dboper.insertDocument(db, {name: "Vadonut", description: "Testsssss"}, 'dishes', (result) => {
+        console.log('Insert document: \n', result.ops );
 
-    collection.insertOne({"name": "Uthapizza", "description" : "Test description alv"}, (err, result) => {
-        assert.strictEqual(err,null);
+        dboper.findDocuments(db, 'dishes', (docs) => {
+            console.log('Found documents: \n', docs);
 
-        console.log('After insrt: \n');
-        console.log(result.ops);
+            dboper.updateDocument(db, {name: 'Vadonut'}, {description: 'Nueva desc alvvv'}, 'dishes', (result) => {
+                console.log('Updated document: \n', result.result);
 
+                dboper.findDocuments(db, 'dishes', (docs) => {
+                    console.log('Found documents: \n', docs);
 
-        collection.find({}).toArray((err, docs) => {
-            assert.strictEqual(err,null);
-            console.log('Found: \n');
-            console.log(docs);
-
-
-            db.dropCollection('dishes', (err, result) => {
-                assert.strictEqual(err,null);
-
-                client.close();
-            });
-        });
-    });
+                    db.dropCollection('dishes', (result) => {
+                        console.log('Dropped collection');
+                        client.close();
+                    })
+                });
+            })
+        })
+    })
 });
