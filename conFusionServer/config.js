40b9dd6fb1cfa9b562e318222c91c049b38487d8
Juan Cazarez <juanjose.cazarezg@gmail.com>
2021-02-26 12:40:37 -0600

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId: '547745129519401',
+        clientSecret: 'c05f019facf480255fa7bb3a2260fe09'
+    }
 }
